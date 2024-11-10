37a282730dda90400fe20807397984ffcc8b992c
Rian <138721470+marshequila@users.noreply.github.com>
2024-11-10 07:32:29 +0100

    flake8 check 12

--- autoop/core/ml/model/__init__.py
+++ autoop/core/ml/model/__init__.py
@@ -1,2 +1,15 @@
+"""
+This package provides machine learning models.
+"""
+
 from .classification import KNearestNeighbors, Logistic, SupportVectorMachine
 from .regression import LassoRegression, MultipleLinearRegression, RandomForest
+
+__all__ = [
+    "KNearestNeighbors",
+    "Logistic",
+    "SupportVectorMachine",
+    "LassoRegression",
+    "MultipleLinearRegression",
+    "RandomForest",
+]
